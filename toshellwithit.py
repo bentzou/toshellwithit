2cfc6e75077e298488cd0d75346cb9dc739eb908
Ben Tzou <bentzou@gmail.com>
2016-05-26 03:00:29 -0700

    make 'help' a method like any other

--- toshellwithit.py
+++ toshellwithit.py
@@ -1,33 +1,44 @@
+from collections import namedtuple
+from collections import OrderedDict
 from inspect import getargspec
 from inspect import getdoc
 from inspect import ismethod
+from itertools import chain
 import json
 import sys
+
+Command = namedtuple('Command', 'method, req_args_list, opt_args_dict')
 
 class ToShellWithIt(object):
    def __init__(self, tool_class, help=True):
       self.tool_class   = tool_class
-      self.commands     = ["help"] if help else []
+      self.commands     = self._get_commands_dict(tool_class)
 
-      if tool_class.__dict__:
-         for method in tool_class.__dict__.keys():
-            if ismethod(getattr(tool_class, method)) and not method.startswith("_"):
-               self.commands.append(method)
-         self.commands.sort()
+      if help:
+         self.commands["help"] = Command(self.print_help, [], {"command": None})
 
-   def _get_signature(self, command):
-      if command == "help":
-         return [], ["command"]
+   def _get_commands_dict(self, tool_class):
+      commands = {}
 
-      argspec = getargspec(getattr(self.tool_class, command))
+      for method_name in tool_class.__dict__.keys():
+         method = getattr(tool_class, method_name)
+         
+         if ismethod(method) and not method_name.startswith("_"):
+            req_args, opt_args = self._get_command_args(tool_class, method_name)
+            commands[method_name] = Command(method, req_args, opt_args)
+
+      return commands
+
+   def _get_command_args(self, tool_class, command):
+      argspec = getargspec(getattr(tool_class, command))
 
       if argspec.defaults:
          num_kw_args = len(argspec.defaults) if argspec.defaults else -1
          req_args = argspec.args[1:-num_kw_args]
-         opt_args = argspec.args[-num_kw_args:]
+         opt_args = OrderedDict(zip(argspec.args[-num_kw_args:], argspec.defaults))
       else:
          req_args = argspec.args[1:]
-         opt_args = []
+         opt_args = OrderedDict()
 
       return req_args, opt_args
 
@@ -36,78 +47,67 @@ class ToShellWithIt(object):
       print("")
       print("Available commands and parameters:")
 
-      for command in self.commands:
-         if False and command == "help":
+      for command, command_info in sorted(self.commands.items()):
+         if command == "help":
             print("   {0:40s} {1}".format("help", "[command]"))
          else:
-            req_args, opt_args = self._get_signature(command)
+            req_args = command_info.req_args_list
+            opt_args = command_info.opt_args_dict
 
             req_args_str = " " + " ".join(req_args) if req_args else ""
-            opt_args_str = " " + " ".join(["[" + opt_arg + "]" for opt_arg in opt_args])
+            opt_args_str = " " + " ".join(["[" + opt_arg + "]" for opt_arg in opt_args.keys()])
 
             print("   {0:40s}{1}{2}".format(command, req_args_str, opt_args_str))
 
       print("")
 
    def print_help(self, command=None):
-      # check correct help usage
-      if command is list and len(command) > 1:
-         raise Exception("Incorrect # of arguments for help\n".format(command))
-
-      # print usage
+      """
+      Prints the docstring associated with the given command.
+      """
       if not command:
          self.usage()
-      elif command in self.tool_class.__dict__:
-         print(getdoc(getattr(self.tool_class, command)))
+      elif command in self.commands:
+         print getdoc(self.commands[command].method)
       else:
          raise Exception("{0} is not a valid command\n".format(command))
 
-   def is_valid_command(self, command):
-      return command == "help" or command in self.tool_class.__dict__
+   def parse_arguments(self, raw_args):
+      args = [arg for arg in raw_args if "=" not in arg]
+      kwargs = dict(tuple(arg.split("=")) for arg in raw_args if "=" in arg)
+
+      return args, kwargs
 
    def run_command(self, cmd, args, kwargs):
-      # get argspec
-      argspec = getargspec(getattr(self.tool_class, cmd))
+      req_args = self.commands[cmd].req_args_list
+      opt_args = self.commands[cmd].opt_args_dict
 
-      if argspec.defaults:
-         req_args = argspec.args[1:-len(argspec.defaults)]
-         opt_args = argspec.args[-len(argspec.defaults):]
-      else:
-         req_args = argspec.args[1:]
-         opt_args = []
-
-      # check if correct # of arguments
-      if len(args) < len(req_args) or len(args) > len(req_args) + len(opt_args):
+      if len(args) < len(req_args) or len(req_args) + len(opt_args) < len(args):
          raise Exception("Incorrect # of arguments for {0}\n".format(cmd))
 
-      return getattr(self.tool_class, cmd)(*args, **kwargs)
+      return self.commands[cmd].method(*args, **kwargs)
 
-   def parse_args(self, arglist):
-      if len(arglist) <= 1:
+   def run(self):
+      # check for command
+      if len(sys.argv) <= 1:
          self.usage()
          sys.exit(0)
 
-      command, arguments = arglist[1], arglist[2:]
-      return command, arguments
-
-   def run(self):
       # get arguments
-      command, arguments = self.parse_args(sys.argv)
+      command, arguments = sys.argv[1], sys.argv[2:]
+      args, kwargs = self.parse_arguments(arguments)
    
       # check if command exists
-      if not self.is_valid_command(command):
+      if command not in self.commands:
          sys.stderr.write("{0} is not a valid command\n".format(command))
          self.usage()
          sys.exit(1)
 
       # run the command
       try:
-         if command == "help":
-            self.print_help(arguments[0] if arguments else None)
-         else:
-            return_value = self.run_command(command, arguments, {})
-            if return_value:
-               print(json.dumps(return_value))
+         return_value = self.run_command(command, args, kwargs)
+         if return_value:
+            print(json.dumps(return_value))
       except Exception, e:
          sys.stderr.write(str(e))
          sys.exit(1)
